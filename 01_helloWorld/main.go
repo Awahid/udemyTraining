4b3f867570ee3d36035c9e2073bf0ecff7828c99
Abdul Wahid <awahid@globalrescue.com>
2018-01-11 16:13:32 +0500

    some new examples has been added till lecture 44

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+
+	fmt.Printf("%v %T \n", a, a)
+	fmt.Printf("%v %T \n", b, b)
+	fmt.Printf("%v %T\n", c, c)
+	fmt.Printf("%v %T \n", d, d)
+	increament := wrapper()
+	fmt.Println(increament())
+
+}
+
+func wrapper() func() int{
+	return func() int{
+		return 2
+	}
 }
